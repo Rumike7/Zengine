fbb80c840cef85e6928d0b78c06479a34d85a70c
Rumike7 <martintossou2@gmail.com>
2025-06-03 13:25:09 +0200

    fix saving ppm loading

--- src/ai.h
+++ src/ai.h
@@ -108,7 +108,7 @@ public:
     color background = color(0.5, 0.7, 1.0);
     double move_speed = 0.1;
     double mouse_sensitivity = 0.005;
-    float vfov = 30;
+    float vfov = 20;
     point3 lookfrom = point3(10,1,0);
     point3 lookat = point3(0, 0, 0);
     vec3 vup = vec3(0, 1, 0);
@@ -119,7 +119,7 @@ public:
     int button_width = 80;
     int button_height = 40;
 
-    camera() : thread_pool(std::thread::hardware_concurrency()){
+    camera() : thread_pool(std::thread::hardware_concurrency()-1){
         if (!initialize()) {
             std::clog << "Camera initialization failed\n";
             throw std::runtime_error("SDL initialization failed");
@@ -169,7 +169,7 @@ public:
             // std::clog << "Frame time: " << delta_time * 1000 << " ms\n";
             
             SDL_Event event;
-            handle_poll_event(event, sc);
+            if(!gui::savingPPM)handle_poll_event(event, sc);
             if(!gui::savingPPM)update_camera();
 
             ImGui_ImplOpenGL3_NewFrame();
@@ -216,42 +216,6 @@ public:
             thread_pool.wait_for_completion();
             thread_pool.reset_completion();
 
-
-            // std::atomic<int> completed_tiles = 0;
-            // int size = 100;
-            // int number_tiles = ((render_height+size-1)/size)*((render_width+size-1)/size);
-            // std::fill(pixel_buffer.begin(), pixel_buffer.end(), color(0, 0, 0));
-            // for (int s_row = 0; s_row < render_height; s_row += size) {
-            //     for(int s_col = 0; s_col < render_width; s_col += size){
-            //         Tile tile = {s_row, std::min(s_row + size, render_height), s_col, std::min(s_col + size, render_width)};
-            //         thread_pool.enqueue([this, &sc, &tile, &format, &completed_tiles]() {
-            //             for (int j = tile.x0; j < tile.x1; ++j) {
-            //                 for (int i = tile.y0; i < tile.y1; ++i) {
-            //                     int idx = j * render_width + i;
-            //                     for (int _ = 0; _ < samples_per_pixel; ++_) {
-            //                         ray r = get_ray(i, j);
-            //                         this->pixel_buffer[idx] += ray_color(r, max_depth, sc);
-            //                     }
-            //                     color c = this->pixel_buffer[idx] * pixel_samples_scale;
-            //                     c = color(sqrt(c.x), sqrt(c.y), sqrt(c.z));
-            //                     this->pixel_data[idx] = SDL_MapRGBA(
-            //                         format,
-            //                         Uint8(clamp(c.x, 0.0, 1.0) * 255.99),
-            //                         Uint8(clamp(c.y, 0.0, 1.0) * 255.99),
-            //                         Uint8(clamp(c.z, 0.0, 1.0) * 255.99),
-            //                         255
-            //                     );
-            //                 }
-            //             }
-            //             completed_tiles++;
-            //         });
-
-            //         // if(s_col >= size*8)break;
-            //     }
-            // }
-            // thread_pool.wait_for_completion();
-            // std::clog <<"Completion : " << completed_tiles << " / " << number_tiles << "\n";
-            // thread_pool.reset_completion();
 
             auto time = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - last_time).count();
             // std::clog << "Compute time " << time*1000 << "ms\n";
@@ -355,7 +319,7 @@ public:
             if (frame_time < target_frame_time) {
                 SDL_Delay(Uint32((target_frame_time - frame_time) * 1000));
             }
-            if (!gui::savingPPM)sc.rebuild_bvh();
+            sc.rebuild_bvh();
         }
 
         SDL_FreeFormat(format);
@@ -456,107 +420,12 @@ private:
     double threshold = 0.001;
     std::vector<color> pixel_buffer;
     std::vector<Uint32> pixel_data;
-    mutable std::mutex camera_mutex;
-
 
 
 
 
     bool initialize() {
-        if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
-            SDL_Log("Error: %s\n", SDL_GetError());
-            return false;
-        }
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-        SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-        
-        SDL_GetWindowSize(gui::window, &window_width, &window_height);
-
-
-        window_height = int(window_width / aspect_ratio);
-        window_height = (window_height < 1) ? 1 : window_height;
-
-        render_width = std::max(400, window_width - gui_width);
-        render_height = int(render_width / aspect_ratio);
-        render_height = std::max(225, std::min(window_height-control_height,render_height));
-        
-
-        std::clog << "Render-screen window size: " << render_width << "x" << render_height << "\n";
-        std::clog << "Full-screen window size: " << window_width << "x" << window_height << "\n";
-
-
-        gui::window = SDL_CreateWindow("Untitled - ZEngine",
-            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height,
-            SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-        SDL_Surface* icon = SDL_LoadBMP("../assets/icon.bmp");
-        if (icon) {
-            SDL_SetWindowIcon(gui::window, icon);
-            SDL_FreeSurface(icon);
-        } else {
-            SDL_Log("Failed to load icon: %s", SDL_GetError());
-        }
-
-        gl_context = SDL_GL_CreateContext(gui::window);
-        SDL_GL_MakeCurrent(gui::window, gl_context);
-        SDL_GL_SetSwapInterval(1);
-
-        if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
-            SDL_Log("Failed to initialize GLAD");
-            return false;
-        }
-
-        IMGUI_CHECKVERSION();
-        ImGui::CreateContext();
-        io = ImGui::GetIO();
-        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-        io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-
-        io.Fonts->AddFontDefault();
-        static const ImWchar icons_ranges[] = { 0xe000, 0xf8ff, 0 };
-        ImFontConfig icons_config;
-        icons_config.MergeMode = true;
-        icons_config.PixelSnapH = true;
-        io.Fonts->AddFontFromFileTTF("../assets/MaterialIcons-Regular.ttf", 16.0f, &icons_config, icons_ranges);
-        io.Fonts->Build();
-
-        glGenTextures(1, &render_texture);
-        glBindTexture(GL_TEXTURE_2D, render_texture);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, render_width, render_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glBindTexture(GL_TEXTURE_2D, 0);
-
-        pixel_buffer.resize(render_width * render_height, color(0, 0, 0));
-        pixel_data.resize(render_width * render_height);
-
-
-
-        ImGui::StyleColorsDark();
-        ImGui_ImplSDL2_InitForOpenGL(gui::window, gl_context);
-        ImGui_ImplOpenGL3_Init("#version 330");
-
-        pixel_samples_scale = 1.0 / samples_per_pixel;
-        vec3 look_dir = unit_vector(lookat - lookfrom);
-
-        pitch = asin(look_dir.y); 
-
-        float cos_pitch = sqrt(1.0 - look_dir.y * look_dir.y); 
-        if (abs(cos_pitch) > 1e-6) { 
-            yaw = atan2(look_dir.z, look_dir.x);//arctan(y/x)
-        } else {
-            yaw = 0.0; 
-        }
-        std::clog << look_dir << " " << yaw << " " << pitch << "\n";
-        update_camera();
-        SDL_SetRelativeMouseMode(SDL_FALSE);        
-        std::cerr << "Camera initialized\n";
-
-        return true;
+        ...
     }
 
     void update_camera() {
@@ -619,20 +488,7 @@ private:
         return ray(ray_origin, ray_direction, ray_time);
     }
 
-    point3 defocus_disk_sample() const {
-        auto p = random_in_unit_disk();
-        return lookfrom + (p.x * defocus_disk_u) + (p.y * defocus_disk_v);
-    }
-
-    vec3 sample_square() const {
-        return vec3(random_double() - 0.5, random_double() - 0.5, 0);
-    }
-
-    double clamp(double x, double min, double max) const {
-        if (x < min) return min;
-        if (x > max) return max;
-        return x;
-    }
+    ...
     
 
    
@@ -673,11 +529,10 @@ private:
             if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED) {
                 window_width = event.window.data1;
                 window_height = event.window.data2;
-                if(!gui::savingPPM)update_render_dimensions();
+                update_render_dimensions();
                 pixel_buffer.resize(render_width * render_height, color(0, 0, 0));
                 pixel_data.resize(render_width * render_height);
             }
-            if(gui::savingPPM)continue;
             if (!io.WantCaptureMouse) {
                 if (event.type == SDL_MOUSEBUTTONDOWN) {
                     if (event.button.button == SDL_BUTTON_LEFT && valid(event.button.x, event.button.y)) {
@@ -774,23 +629,7 @@ private:
 
 
     
-    void update_render_dimensions() {
-        render_width = std::max(400, window_width - gui_width);
-        render_height = int(render_width / aspect_ratio);
-        render_height = std::max(225, std::min(window_height-control_height,render_height));
-
-
-        std::clog << "Render-screen window size: " << render_width << "x" << render_height << "\n";
-
-        glBindTexture(GL_TEXTURE_2D, render_texture);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, render_width, render_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glBindTexture(GL_TEXTURE_2D, 0);
-
-        update_camera();
-    }
-    
+    ...
 
 };
 
